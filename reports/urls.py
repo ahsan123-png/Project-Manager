cc4155ab17ef0e985b2c6a42afd63c833816a573
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2025-06-22 18:26:42 +0500

    Refactor URL patterns in reports app for clarity; comment out unused paths for future implementation

--- reports/urls.py
+++ reports/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import ReportCreateView, ReportDownloadView, ReportDetailView
+from .views import ReportCreateView
 
 urlpatterns = [
-    path('', ReportCreateView.as_view(), name='report-create'),
-    path('<uuid:pk>/', ReportDetailView.as_view(), name='report-detail'),
-    path('<uuid:pk>/download/', ReportDownloadView.as_view(), name='report-download'),
+    path('', ReportCreateView.as_view(), name='report-create')
+    # path('<uuid:pk>/', ReportDetailView.as_view(), name='report-detail'),
+    # path('<uuid:pk>/download/', ReportDownloadView.as_view(), name='report-download'),
 ]
--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from .models import Task , Milestone
-
+from drf_spectacular.utils import extend_schema_field
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = '__all__'
+        ref_name = 'TaskSerializer'
         # read_only_fields = ('id', 'project', 'assigned_to', 'deadline', 'title', 'description')
 
 class MilestoneSerializer(serializers.ModelSerializer):
@@ -15,7 +16,15 @@ class MilestoneSerializer(serializers.ModelSerializer):
         fields = '__all__'
         read_only_fields = ('id', 'created_at', 'updated_at')
 
-    def get_manager(self, obj):
+    @extend_schema_field({
+        'type': 'object',
+        'properties': {
+            'id': {'type': 'integer'},
+            'name': {'type': 'string'},
+            'email': {'type': 'string', 'format': 'email'},
+        }
+    })
+    def get_manager(self, obj) -> dict:
         """Include manager information from the project"""
         return {
             'id': obj.project.manager.id,
